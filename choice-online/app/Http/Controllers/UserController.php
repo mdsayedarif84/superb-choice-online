8759a3932e0c1f25f4ed24eaebb032cbd1988f18
mdsayedarif84 <mdsayedarif84@gmail.com>
2022-10-10 08:36:10 +0600

    UserController Authtype edit

--- choice-online/app/Http/Controllers/UserController.php
+++ choice-online/app/Http/Controllers/UserController.php
@@ -11,28 +11,28 @@ use Illuminate\Support\Facades\Session;
 class UserController extends Controller{
    public function manageUser(){
    // First way get data
-      // $userId=  Auth::user()->id;
-      // $authType=  Auth::user()->auth_type;
-      // if($authType =="user" ){
-      //       $users =DB::table('users')
-      //                ->where(['id'=>$userId])
-      //                ->get('users.*');
-      //                dd($users);
-      //    return view('admin.user.manage-user',compact('users'));
-      // }else{
-      //    $users     =   User::all();
-      //    return view('admin.user.manage-user',compact('users'));
-      // }
-         // 2nd way get data
-      $userId=Session::get('userId');
-      $user =User::where('id',$userId)->get()->first();
-      if($user->auth_type =="user" ){
-         $users =User::where('id',$user->id)->get();
-            // $users =$user;
+      $userId=  Auth::user()->id;
+      $authType=  Auth::user()->auth_type;
+      if($authType =="user" ){
+            $users =DB::table('users')
+                     ->where(['id'=>$userId])
+                     ->get('users.*');
+                     // dd($users);
          return view('admin.user.manage-user',compact('users'));
       }else{
          $users     =   User::all();
          return view('admin.user.manage-user',compact('users'));
       }
+         // 2nd way get data
+      // $userId=Session::get('userId');
+      // $user =User::where('id',$userId)->get()->first();
+      // if($user->auth_type =="user" ){
+      //    $users =User::where('id',$user->id)->get();
+      //       // $users =$user;
+      //    return view('admin.user.manage-user',compact('users'));
+      // }else{
+      //    $users     =   User::all();
+      //    return view('admin.user.manage-user',compact('users'));
+      // }
    }
 }
